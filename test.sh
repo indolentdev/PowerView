c954e18eb2c684ac501fbcc72bae80811a402af1
indolentdev <39415815+indolentdev@users.noreply.github.com>
2020-04-05 21:04:01 +0200

    Upgraded nuget packages

--- test.sh
+++ test.sh
@@ -5,4 +5,4 @@ if [ $? -ne 0 ]
 then
   exit 1
 fi
-mono packages/NUnit.ConsoleRunner.3.10.0/tools/nunit3-console.exe PowerView.Model.Test/bin/Debug/PowerView.Model.Test.dll PowerView.Service.Test/bin/Debug/PowerView.Service.Test.dll PowerView.Test/bin/Debug/PowerView.Test.dll
+mono packages/NUnit.ConsoleRunner.3.11.1/tools/nunit3-console.exe PowerView.Model.Test/bin/Debug/PowerView.Model.Test.dll PowerView.Service.Test/bin/Debug/PowerView.Service.Test.dll PowerView.Test/bin/Debug/PowerView.Test.dll
