bf1f11f38213f7235c584635cfac38ed8281ad57
indolentdev <39415815+indolentdev@users.noreply.github.com>
2022-08-24 21:08:04 +0200

    Leverage Microsoft.Data.Sqlite to write DateTime to the database.

--- PowerView.Model/Repository/DapperConfig.cs
+++ PowerView.Model/Repository/DapperConfig.cs
@@ -17,15 +17,18 @@ public static class DapperConfig
 
     /// <summary>
     /// Microsoft.Data.Sqlite defaults to string type in the database and DateTimeKind.Unspecified.
-    /// PowerView uses invariant UTC. And the database historically uses unix timestamps (integer), from when "System.Data.SQLite" was used  on mono.
-    /// So comply with that.
+    /// PowerView uses invariant UTC.
+    /// So bridge the two.
     /// </summary>
     private class DateTimeUtcHandler : SqlMapper.TypeHandler<DateTime>
     {
         public override void SetValue(IDbDataParameter parameter, DateTime value) 
         {
             if (value.Kind != DateTimeKind.Utc) throw new ArgumentOutOfRangeException(nameof(value), $"Must be UTC. Was:{value.Kind}");
-            parameter.Value = value.ToString("o", CultureInfo.InvariantCulture);
+
+            // Leave it to the provider to convert DateTime to string:
+            // https://github.com/dotnet/efcore/blob/release/7.0/src/Microsoft.Data.Sqlite.Core/SqliteValueBinder.cs#L97
+            parameter.Value = value;
         }
 
         public override DateTime Parse(object value)
