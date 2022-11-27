af8020905f81e3c581c6039371d5eb70d6eff279
indolentdev <39415815+indolentdev@users.noreply.github.com>
2022-11-27 21:50:49 +0100

    Disable RPi deploy

--- deploy.sh
+++ deploy.sh
@@ -88,11 +88,7 @@ fi
 md5sum $zipname > $md5name
 
 # Uncomment next line to exit script after zipping the release
-<<<<<<< Updated upstream
-#exit 1
-=======
 exit 1
->>>>>>> Stashed changes
 
 # Prepare the folder on remote system
 echo Deleting temporary folder $name on remote system
